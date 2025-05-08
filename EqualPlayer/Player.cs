d63ed0219eb34ce51c5256a63ca18470f168fb71
Drackout <mikatx2@hotmail.com>
2025-05-08 23:08:32 +0100

    Ex4 makes no sense, why would any1 use that

--- EqualPlayer/Player.cs
+++ EqualPlayer/Player.cs
@@ -15,5 +15,20 @@ namespace EqualPlayer
             Name = name ;
         }
 
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //since we just want it to always insert if is equal..?
+            return false;
+        }
+        
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // I HAVE NO IDEA, THIS MAKES NO SENESE AT ALLLLLLL
+            throw new System.NotImplementedException();
+            return base.GetHashCode();
+        }
+
     }
 }
